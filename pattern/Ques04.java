0d4d4315bbcdc312eea84fb7a1ad24ae86c0db6e
Anubhav Ranjan <anbhavranjan2525@gmail.com>
2024-09-23 19:55:30 +0530

    updated set

--- pattern/Ques04.java
+++
@@ -1,9 +0,0 @@
-public class Ques04 {
-    public static void main(String[] args) {
-        for (int i = 4; i >= 1; i--) {
-            for (int j = i; j <= 4; j++)
-                System.out.print(j);
-            System.out.println();
-        }
-    }
-}
